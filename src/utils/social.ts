d3bda0f6a940855fb6a2fc538ec3468789642a04
Husein Kantarci <huseinkantarci@gmail.com>
2024-11-20 15:42:21 +0100

    Update social.ts

--- src/utils/social.ts
+++ src/utils/social.ts
@@ -1,6 +1,5 @@
 export const SOCIAL_LINKS = {
   GITHUB: 'https://github.com/huseink',
   LINKEDIN: 'https://www.linkedin.com/in/huseinkantarci/',
-  TWITTER: 'https://twitter.com/huseinkntrc',
   INSTAGRAM: 'https://www.instagram.com/huseinkntrc/',
 }
