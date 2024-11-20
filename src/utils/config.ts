974defcc545a76fc44803433092322f18a4c3144
Husein Kantarci <huseinkantarci@gmail.com>
2024-11-20 15:20:12 +0100

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
-  site_name: 'Husein Kantarci - Developer',
-  title: 'Husein Kantarci - Developer',
+  site_name: 'Husein Kantarci - Full Stack Software Engineer',
+  title: 'Husein Kantarci - Full Stack Software Engineer',
   description: 'Personal portfolio',
   blogTitle: 'Blog | Husein Kantarci',
   snippetsTitle: 'Snippets | Husein Kantarci',
