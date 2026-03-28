26ea8d97d9a1c19955fc7259140b2305e92843fb
huseinkntrc <husein@mojob.io>
2026-03-28 02:24:29 +0100

    refine portfolio design and content

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
-  site_name: 'Husein Kantarci - Full Stack Software Engineer',
-  title: 'Husein Kantarci - Full Stack Software Engineer',
+  site_name: 'Husein Kantarci - Software Engineer',
+  title: 'Husein Kantarci - Software Engineer',
   description: 'Personal portfolio',
   blogTitle: 'Blog | Husein Kantarci',
   snippetsTitle: 'Snippets | Husein Kantarci',
